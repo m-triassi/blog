ffb44d85a4890fbbd26fc9f2f95f9c67046a3dcd
Massimo Triassi <kidmax14@hotmail.com>
2024-11-20 15:54:57 -0500

    fix: use proper site url in config to make images show

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,7 +5,7 @@ import tailwind from '@astrojs/tailwind';
 
 // https://astro.build/config
 export default defineConfig({
-    site: 'https://example.com',
+    site: 'https://blog.triassi.ca',
     integrations: [
         mdx(),
         sitemap(),
